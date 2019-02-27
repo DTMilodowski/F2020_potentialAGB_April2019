6768ad6c86a59eff486fa7d87a8ee612375d2d93
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-02-27 14:24:02 +0000

    some detailed comments, and removing redundant function

--- src/data_io/data_io.py
+++ src/data_io/data_io.py
@@ -1,4 +1,8 @@
 """
+27/02/2019 - DTM
+Paring back scripts to the data io routines required in the potential biomass
+estimation.
+
 30/11/2018 - DTM
 Rewritten some of the functions specific to Forests2020 potential biomass work
 - no LUH data
@@ -16,29 +20,50 @@ import numpy as np
 import sys
 import set_training_areas
 import rasterio
-import forestci as fci
 from copy import deepcopy
 
-# Load predictor variables
-def get_predictors(country_code,return_landmask = True, training_subset=False, subset_def=1):
+"""
+load_predictors
+This function loads all of the datasets containign the explanatory variables,
+and applies a mask so that only land areas are considered, and any nodata values
+are removed.
 
-    path = '/disk/scratch/local.2/dmilodow/PotentialBiomass/processed/%s/' % country_code
-    path2wc = path+'wc2/'
-    path2sg = path+'soilgrids/'
-    path2agb = path+'agb/'
+The function takes one input argument:
+    path2root
+This is the file path to the root directory for the workshop, and can be
+expressed either as a full path or relative path. It is needed so that the code
+knows exactly where things are stored - note the correct directory structure is
+required!!!
+By default, we assume that the working directory is the "src" directory, in
+which case the path2root is simply "../"
 
-    #worldclim2 data
+The function returns two objects:
+    1) predictors: a large 2D numpy array where the rows correspond with land
+       pixels and the columns correspond with each successive data set.
+    2) landmask: a boolian array which dimensions (n_latitude,n_longitude)
+       where land pixels are marked by ones, and water bodies/nodata pixels
+       are marked by zeros
+"""
+def load_predictors(path2root = "../"):
+
+    # Path structures
+    path2wc = path2root+'/data/climatology/'
+    path2sg = path2root+'/data/soils/'
+    path2agb = path2root+'data/agb/'
+
+    # Load the worldclim2 data
     nodata=[]
     for ff in sorted(glob.glob(path2wc+'*tif')):
         nodata.append(rasterio.open(ff).nodatavals[0])
 
     wc2 = xr.concat([xr.open_rasterio(f) for f in sorted(glob.glob(path2wc+'*tif'))],dim='band')
-    wc2_mask = wc2[0]!=nodata[0]#wc2[0]!=wc2[0,0,0]
+    wc2_mask = wc2[0]!=nodata[0]
     for ii in range(wc2.shape[0]):
-        wc2_mask = wc2_mask & (wc2[ii]!=nodata[ii])#(wc2[ii]!=wc2[ii,0,0])
+        wc2_mask = wc2_mask & (wc2[ii]!=nodata[ii])
     print('Loaded WC2 data')
 
-    #soilgrids data - filter out a bunch of variables correlated with land cover
+    # Load the soilgrids data
+    # Note that we filter out a bunch of variables correlated with land cover
     soilfiles_all = glob.glob(path2sg+'*tif')
     soilfiles = []
     #             %sand %silt %clay %D2Rhorizon %probRhorizon %D2bedrock
@@ -52,27 +77,18 @@ def get_predictors(country_code,return_landmask = True, training_subset=False, s
         nodata.append(rasterio.open(ff).nodatavals[0])
 
     soil= xr.concat([xr.open_rasterio(f) for f in sorted(soilfiles)],dim='band')
-    soil_mask = soil[0]!=nodata[0]#soil[0]!=soil[0,0,0]
+    soil_mask = soil[0]!=nodata[0]
     for ii in range(soil.shape[0]):
-        soil_mask = soil_mask & soil[ii]!=nodata[0]#(soil[ii]!=soil[ii,0,0])
+        soil_mask = soil_mask & soil[ii]!=nodata[0]
     print('Loaded SOILGRIDS data')
 
-    #also load the AGB data to only perform the PCA for places where there is both AGB and uncertainty
+    #also load the AGB data to check we only keep pixels with AGB estimates
     agb_file = '%sAvitabile_AGB_%s_1km.tif' % (path2agb,country_code)
     agb = xr.open_rasterio(agb_file)
     agb_mask = agb.values[0]!=np.float32(agb.nodatavals[0])
 
-    unc_file = '%sAvitabile_AGB_%s_1km.tif' % (path2agb,country_code)
-    unc = xr.open_rasterio(unc_file)
-    unc_mask = unc[0]!=unc.nodatavals[0]
-
-    #create the land mask knowing that top left pixels (NW) are all empty
-    # for now, ignoring uncertainty as want to include N. Australia in training
-    if training_subset:
-        training_mask = set_training_areas.set(path,subset=subset_def)
-        landmask = (training_mask & wc2_mask.values & soil_mask.values & agb_mask)# & unc_mask.values)
-    else:
-        landmask = (wc2_mask.values & soil_mask.values & agb_mask)# & unc_mask.values)
+    #create the land mask by combining the nodata masks for all data sources
+    landmask = (wc2_mask.values & soil_mask.values & agb_mask)
 
     #create the empty array to store the predictors
     predictors = np.zeros([landmask.sum(),soil.shape[0]+wc2.shape[0]])
@@ -88,89 +104,15 @@ def get_predictors(country_code,return_landmask = True, training_subset=False, s
     for bi in wc2:
         predictors[:,counter] = bi.values[landmask]
         counter += 1
-    print('Extracted WC2 data')
+
     #then soil properties
     for sp in soil:
         predictors[:,counter] = sp.values[landmask]
         counter += 1
-    print('Extracted SOILGRIDS data')
+    print('Extracted WorldClim2 and SOILGRIDS data')
 
-    if return_landmask:
-        return(predictors,landmask)
-    else:
-        return(predictors)
+    return(predictors,landmask)
 
-# equivalent function to above, but for specified mask, rather than creating mask within function
-def get_predictors_for_defined_mask(country_code,mask):
-
-    path = '/disk/scratch/local.2/dmilodow/PotentialBiomass/processed/%s/' % country_code
-    path2wc = path+'wc2/'
-    path2sg = path+'soilgrids/'
-    path2agb = path+'agb/'
-
-    #worldclim2 data
-    nodata=[]
-    for ff in sorted(glob.glob(path2wc+'*tif')):
-        nodata.append(rasterio.open(ff).nodatavals[0])
-
-    wc2 = xr.concat([xr.open_rasterio(f) for f in sorted(glob.glob(path2wc+'*tif'))],dim='band')
-    wc2_mask = wc2[0]!=nodata[0]#wc2[0]!=wc2[0,0,0]
-    for ii in range(wc2.shape[0]):
-        wc2_mask = wc2_mask & (wc2[ii]!=nodata[ii])#(wc2[ii]!=wc2[ii,0,0])
-    print('Loaded WC2 data')
-
-    #soilgrids data - filter out a bunch of variables correlated with land cover
-    soilfiles_all = glob.glob(path2sg+'*tif')
-    soilfiles = []
-    #             %sand %silt %clay %D2Rhorizon %probRhorizon %D2bedrock
-    filtervars = ['SNDPPT','SLTPPT','CLYPPT','BDRICM','BDRLOG','BDTICM']
-    for ff in range(len(soilfiles_all)):
-        if soilfiles_all[ff].split('/')[-1].split('.')[0].split('_')[0] in filtervars:
-            soilfiles.append(soilfiles_all[ff])
-
-    nodata=[]
-    for ff in sorted(soilfiles):
-        nodata.append(rasterio.open(ff).nodatavals[0])
-
-    soil= xr.concat([xr.open_rasterio(f) for f in sorted(soilfiles)],dim='band')
-    soil_mask = soil[0]!=nodata[0]#soil[0]!=soil[0,0,0]
-    for ii in range(soil.shape[0]):
-        soil_mask = soil_mask & soil[ii]!=nodata[0]#(soil[ii]!=soil[ii,0,0])
-    print('Loaded SOILGRIDS data')
-
-    #also load the AGB data to only perform the PCA for places where there is both AGB and uncertainty
-    agb_file = '%sAvitabile_AGB_%s_1km.tif' % (path2agb,country_code)
-    agb = xr.open_rasterio(agb_file)
-    agb_mask = agb.values[0]!=np.float32(agb.nodatavals[0])
-
-    unc_file = '%sAvitabile_AGB_%s_1km.tif' % (path2agb,country_code)
-    unc = xr.open_rasterio(unc_file)
-    unc_mask = unc[0]!=unc.nodatavals[0]
-
-    landmask = (mask & wc2_mask.values & soil_mask.values & agb_mask)# & unc_mask.values)
-
-    #create the empty array to store the predictors
-    predictors = np.zeros([landmask.sum(),soil.shape[0]+wc2.shape[0]])
-
-    # check the mask dimensions
-    if len(landmask.shape)>2:
-        print('\t\t caution shape of landmask is: ', landmask.shape)
-        landmask = landmask[0]
-
-    #iterate over variables to create the large array with data
-    counter = 0
-    #first wc2
-    for bi in wc2:
-        predictors[:,counter] = bi.values[landmask]
-        counter += 1
-    print('Extracted WC2 data')
-    #then soil properties
-    for sp in soil:
-        predictors[:,counter] = sp.values[landmask]
-        counter += 1
-    print('Extracted SOILGRIDS data')
-
-    return(predictors)
 
 # Get a defined mask
 # - country code is a prefix identifying the study area
@@ -227,187 +169,3 @@ def get_mask(country_code, mask_def=0):
         mask = mask[0]
 
     return mask
-
-
-#===============================================================================
-# AUXILLIARY FUNCTIONS TO CALCULATE USEFUL INFORMATION
-#-------------------------------------------------------------------------------
-# get areas for a global grid with 0.25x0.25 res
-# edited to take in  optional grid dimensions
-default_lat = np.arange(90-1/8.,-90.,-1/4.)
-default_lon = np.arange(-180+1/8.,180.,1/4.)
-def get_areas(latorig = default_lat, lonorig = default_lon):
-    #latorig = np.arange(90-1/8.,-90.,-1/4.)
-    #lonorig = np.arange(-180+1/8.,180.,1/4.)
-    areas = np.zeros([latorig.size,lonorig.size])
-    res = np.abs(latorig[1]-latorig[0])
-    for la,latval in enumerate(latorig):
-        areas[la]= (6371e3)**2 * ( np.deg2rad(0+res/2.)-np.deg2rad(0-res/2.) ) * (np.sin(np.deg2rad(latval+res/2.))-np.sin(np.deg2rad(latval-res/2.)))
-
-    return areas
-
-"""
-#===============================================================================
-# FUNCTIONS THAT ARE CALLED DURING THE ITERATIVE TRAINING DATA SELECTION PROCESS
-#-------------------------------------------------------------------------------
-"""
-# This uses the confidence intervals from the random forest to trim the training
-# set
-def iterative_augmentation_of_training_set_rfci(ytest, y, Xtest, X, Xall, iterations,
-                                            landmask, initial_training_mask,
-                                            other_stable_forest_mask, rf, yscaler,
-                                            memory_limit = 8000):
-    # define host array for potential biomass estimates at each iteration
-    AGBpot = np.zeros((iterations,landmask.shape[0],landmask.shape[1]))*np.nan
-    AGBpot[0][landmask] = yscaler.inverse_transform(rf.predict(Xall).reshape(-1, 1)).ravel()
-
-    # Also track which pixels are included in the additional training set
-    # pixels marked 1 are in the initial training set
-    # pixels marked 2 are in the additional training set
-    training_set = np.zeros((iterations,landmask.shape[0],landmask.shape[1]))
-    training_set[0] = initial_training_mask.copy()
-
-    # keep track of row and column indices for updating training set maps
-    cols,rows = np.meshgrid(np.arange(landmask.shape[1]),np.arange(landmask.shape[0]))
-    col_idx = cols[other_stable_forest_mask]
-    row_idx = rows[other_stable_forest_mask]
-
-    # now iterate, stopping after specified number of iterations, or if
-    # training set stabilises sooner
-    print("starting iterative training selection")
-    ii=1
-    while(ii<iterations):
-        # track number of additional training pixels considered in this iteration
-        n_additional = ytest.size
-        # predict potential biomass
-        # - (ii) for additional training subset
-        ytest_pr = rf.predict(Xtest)
-        # calculate variance in estimate for this subset
-        var = fci.random_forest_error(rf, X, Xtest, memory_constrained = True,
-                                        memory_limit = memory_limit)
-        # Filter out pixels with biomass below potential (accounting for error)
-        subset = (ytest[:,0]+np.sqrt(var)) >= ytest_pr
-
-        # Check how many pixels were removed from the additional training set
-        n_removed = n_additional-subset.sum()
-        print("Iteration %i complete, removed %i out of %i pixels from training" %
-                (ii+1,n_removed, n_additional))
-        # repeat iterations unit either n_iter is reached, or reached steady state
-        if(n_removed==0):
-            # skip to end of loop
-            ii = iterations
-        else:
-            # record AGBpot & training set for this next iteration
-            AGBpot[ii][landmask] = yscaler.inverse_transform(rf.predict(Xall).reshape(-1, 1)).ravel()
-            training_set[ii] = initial_training_mask.copy()
-            training_set[ii,row_idx,col_idx] = 2
-
-            # refine subset
-            ytest = ytest[subset]
-            Xtest = Xtest[subset,:]
-            col_idx = col_idx[subset]
-            row_idx = row_idx[subset]
-
-            # fit new random forest with new training subset
-            Xiter=np.concatenate((X,Xtest),axis=0)
-            yiter=np.concatenate((y,ytest),axis=0)
-            rf.fit(Xiter,yiter)
-            ii+=1
-    np.save('AGBpot_test.npy',AGBpot)
-    np.save('training_test.npy',training_set)
-    return AGBpot, training_set, rf
-
-# This version is simpler - it simply filters out additional stable forest for
-# which the AGBobs < AGBpot in each iteration. It is also much faster! Unlike
-# the previous function, it does not need the data to be scaled, either
-def iterative_augmentation_of_training_set_obs_vs_pot(ytest, y, Xtest, X, Xall, iterations,
-                                            landmask, initial_training_mask,
-                                            other_stable_forest_mask, rf,stopping_condition=0.05):
-    # define host array for potential biomass estimates at each iteration
-    AGBpot = np.zeros((iterations+1,landmask.shape[0],landmask.shape[1]))*np.nan
-    AGBpot[0][landmask] = rf.predict(Xall)
-
-    # Also track which pixels are included in the additional training set
-    # pixels marked 1 are in the initial training set
-    # pixels marked 2 are in the additional training set
-    training_set = np.zeros((iterations+1,landmask.shape[0],landmask.shape[1]))
-    training_set[0] = initial_training_mask.copy()
-
-    # keep track of row and column indices for updating training set maps
-    cols,rows = np.meshgrid(np.arange(landmask.shape[1]),np.arange(landmask.shape[0]))
-    col_idx = cols[other_stable_forest_mask]
-    row_idx = rows[other_stable_forest_mask]
-
-    # now iterate, stopping after specified number of iterations, or if
-    # training set stabilises sooner
-    print("starting iterative training selection")
-
-    # predict potential biomass for additional training subset
-    ytest_pr = rf.predict(Xtest)
-    # calculate RMSE of subset
-    rmse_pr = np.sqrt(np.mean((ytest_pr-ytest)**2))
-    # copy rf model
-    rf_pr = deepcopy(rf)
-
-    ii=0
-    while(ii<iterations):
-        # track number of additional training pixels considered in this iteration
-        n_additional = ytest.size
-        # Filter out pixels with biomass below potential (accounting for error)
-        residual = ytest - ytest_pr
-        residual_as_fraction = residual/ytest
-        threshold = np.percentile(residual_as_fraction,25)
-
-        #subset = (ytest >= ytest_pr)
-        subset = np.all((residual_as_fraction >= threshold,ytest >= ytest_pr),axis=0)
-
-        # refine subset
-        ytest = ytest[subset]
-        Xtest = Xtest[subset,:]
-        col_idx = col_idx[subset]
-        row_idx = row_idx[subset]
-
-        # fit new random forest with new training subset
-        Xiter=np.concatenate((X,Xtest),axis=0)
-        yiter=np.concatenate((y,ytest),axis=0)
-        rf.fit(Xiter,yiter)
-
-        # record AGBpot & training set for this next iteration
-        AGBpot[ii+1][landmask] = rf.predict(Xall)
-        training_set[ii+1] = initial_training_mask.copy()
-        training_set[ii+1,row_idx,col_idx] = 2
-
-
-        # predict potential biomass for additional training subset
-        ytest_pr = rf.predict(Xtest)
-        # calculate RMSE of subset
-        rmse = np.sqrt(np.mean((ytest_pr-ytest)**2))
-
-        # Check how many pixels were removed from the additional training set
-        n_removed = n_additional-subset.sum()
-        print("Iteration %i complete, removed %i out of %i pixels from training, %.1f" %
-                (ii+1,n_removed, n_additional,float(n_removed)/float(n_additional)*100.))
-        print("RMSE previous: %.02f; RMSE updated: %.02f; percentage change: %.02f" %
-                (rmse_pr, rmse,(rmse-rmse_pr)/(rmse_pr)*100.))
-
-        # repeat iterations unit either n_iter is reached, or reached steady state
-        #if(float(n_remove)/float(n_additional)<stopping_condition):
-        if((rmse_pr-rmse)/(rmse_pr)<=stopping_condition):
-            # if rmse increases, revert back to previous iteration
-            if((rmse_pr-rmse)/(rmse_pr)<=0) :
-                AGBpot=AGBpot[:ii+2]
-                training_set = training_set[:ii+2]
-                #rf=deepcopy(rf_pr)
-            else:
-                AGBpot=AGBpot[:ii+2]
-                training_set = training_set[:ii+2]
-            # skip to end of loop
-            ii = iterations
-        else:
-            rf_pr = deepcopy(rf)
-            rmse_pr = rmse
-            n_additional_previous = n_additional
-            ii+=1
-    np.save('AGBpot_test.npy',AGBpot)
-    np.save('training_test.npy',training_set)
-    return AGBpot, training_set, rf
