597f3a6d21f202cedd3cca542d2bddb261301547
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-03-25 14:17:18 +0000

    simple spatial plotting script

---
+++ src/data_visualisation/map_plots.py
@@ -0,0 +1,14 @@
+"""
+map_plots
+--------------------------------------------------------------------------------
+GENERATE MAP PLOTS FOR WORKSHOP
+# David T. Milodowski, 25/03/2019
+"""
+
+"""
+import libraries needed
+"""
+import numpy as np
+import matplotlib.pyplot as plt     # plotting package
+import seaborn as sns               # another useful plotting package
+sns.set()
