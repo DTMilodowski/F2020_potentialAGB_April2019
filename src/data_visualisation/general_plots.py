fac829399ee94276c270b2bae624da293191febb
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-03-25 16:51:54 +0000

    calval plot

--- src/data_visualisation/general_plots.py
+++ src/data_visualisation/general_plots.py
@@ -11,6 +11,8 @@ import libraries needed
 import numpy as np
 import matplotlib.pyplot as plt     # plotting package
 import seaborn as sns               # another useful plotting package
+import pandas as pd
+from sklearn.metrics import r2_score, mean_squared_error
 
 """
 Part 2: Random forests
@@ -46,3 +48,30 @@ def plot_test_data_with_regression_results(X1,y1,X2,y2,X3,y3,X_test,y1_test,y1_t
     if show:
         fig2.show()
     return fig2,axes
+
+# Figure 3, basic cal-val example
+def plot_cal_val(y_train,y_train_rf,y_test,y_test_rf,show=True):
+    calval_df = pd.DataFrame(data = {'val_obs': y_test,
+                                     'val_model': y_test_rf,
+                                     'cal_obs': y_train,
+                                     'cal_model': y_train_rf})
+
+    fig3,axes= plt.subplots(nrows=1,ncols=2,figsize=[8,4])
+    sns.regplot(x='cal_obs',y='cal_model',data=calval_df,marker='+',
+                truncate=True,ci=None,ax=axes[0])
+    axes[0].annotate('calibration R$^2$ = %.02f\nRMSE = %.02f' %
+                (r2_score(y_train,y_train_rf),np.sqrt(mean_squared_error(y_train,y_train_rf))),
+                xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',
+                horizontalalignment='left', verticalalignment='top')
+    sns.regplot(x='val_obs',y='val_model',data=calval_df,marker='+',
+                truncate=True,ci=None,ax=axes[1])
+    axes[1].annotate('validation R$^2$ = %.02f\nRMSE = %.02f'
+                % (r2_score(y_test,y_test_rf),np.sqrt(mean_squared_error(y_test,y_test_rf))),
+                xy=(0.05,0.95), xycoords='axes fraction',backgroundcolor='none',
+                horizontalalignment='left', verticalalignment='top')
+    axes[0].axis('equal')
+    axes[1].axis('equal')
+    fig3.tight_layout()
+    if show:
+        fig3.show()
+    return fig3,axes
