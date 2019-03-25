4b9ec7bfa87159c0c72e1f90f497947732afd79e
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-03-25 16:52:37 +0000

    some notes

--- src/random_forest/utility.py
+++ src/random_forest/utility.py
@@ -5,6 +5,12 @@ Miscellaneous useful functions
 David T. Milodowski, 25/03/2019
 """
 import numpy as np
+
+"""
+generate_simple_test_data
+-----------------------------------------
+Genereate three contrasting datasets on which to conduct simple tests
+"""
 def generate_simple_test_data():
     X1 = np.random.random(500)*10. # generate a set of random numbers between 0 and 10
     y1= 2.3*X1 + np.random.randn(500)*1. # generate a linear relationship with some noise - here noise comes from a random distribution with mean 0 and standard deviation of 1.
